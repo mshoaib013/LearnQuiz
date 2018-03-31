677e17ac261677061f9c26ff3cfc3bc8646f32c8
darkshadow <shoaib.mehedi@gmail.com>
2018-04-01 03:35:20 +0600

    Initial commit

---
+++ app/src/main/java/com/example/darkshadow/learnquiz/MainActivity.java
@@ -0,0 +1,231 @@
+package com.example.darkshadow.learnquiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private ArrayAdapter<String> listAdapter ;
+    private int viewno,chapterOnGoing;
+    private ListView listView;
+    ScrollView tutorialview;
+    ListView chapterlistview;
+    ImageView backword,forward;
+    TextView tutorial;
+    TextView chaptername;
+    Button gridOne;
+    LinearLayout menuGridView;
+    strings strings=new strings();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        listView = (ListView) findViewById( R.id.chapterlistview );
+        tutorialview=(ScrollView) findViewById(R.id.tutorialview);
+        chapterlistview=(ListView) findViewById(R.id.chapterlistview);
+        backword=(ImageView) findViewById(R.id.bacward);
+        forward=(ImageView) findViewById(R.id.forward);
+        tutorial=(TextView) findViewById(R.id.tutorial);
+        chaptername=(TextView) findViewById(R.id.chaptername);
+        gridOne=(Button) findViewById(R.id.gridOne);
+        menuGridView=(LinearLayout) findViewById(R.id.menu);
+        viewno=0;
+
+        String[] chapters = new String[] { "Chapter one", "Chapter two", "Chapter three", "Mars",
+                "Jupiter", "Saturn", "Uranus", "Neptune"};
+        final ArrayList<String> chapterlist = new ArrayList<String>();
+        chapterlist.addAll(Arrays.asList(chapters));
+        listAdapter = new ArrayAdapter<String>(this, R.layout.listview, chapterlist);
+        listView.setAdapter(listAdapter);
+
+
+
+        //
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //switchView();
+                menuGridView.setVisibility(View.GONE);
+                chapterlistview.setVisibility(View.GONE);
+                tutorialview.setVisibility(View.VISIBLE);
+                viewno=2;
+                chaptername.setText(strings.chapter[position][0]);
+                tutorial.setText(strings.chapter[position][1]);
+                tutorialview.setScrollY(0);
+                chapterOnGoing=position;
+
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        backword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (chapterOnGoing>0){
+                    chapterOnGoing=chapterOnGoing-1;
+                    backwordForwardController(0);
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"Nothing Before",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        forward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (chapterOnGoing<4){
+                    chapterOnGoing=chapterOnGoing+1;
+                    backwordForwardController(1);
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"Nothing Next",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        gridOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menuGridView.setVisibility(View.GONE);
+                chapterlistview.setVisibility(View.VISIBLE);
+                tutorialview.setVisibility(View.GONE);
+                viewno=1;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else if (viewno==2){
+            chapterlistview.setVisibility(View.VISIBLE);
+            tutorialview.setVisibility(View.GONE);
+            menuGridView.setVisibility(View.GONE);
+            viewno=1;
+        }
+        else if (viewno==1){
+            chapterlistview.setVisibility(View.GONE);
+            tutorialview.setVisibility(View.GONE);
+            menuGridView.setVisibility(View.VISIBLE);
+            viewno=0;
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void switchView(){
+        if(tutorialview.getVisibility()==View.GONE && chapterlistview.getVisibility()==View.GONE){
+            tutorialview.setVisibility(View.GONE);
+            chapterlistview.setVisibility(View.VISIBLE);
+            menuGridView.setVisibility(View.GONE);
+            viewno=0;
+        }
+        else if (menuGridView.getVisibility()==View.GONE && tutorialview.getVisibility()==View.GONE){
+            tutorialview.setVisibility(View.GONE);
+            chapterlistview.setVisibility(View.VISIBLE);
+            menuGridView.setVisibility(View.GONE);
+            viewno=1;
+        }
+        else {
+            menuGridView.setVisibility(View.GONE);
+            chapterlistview.setVisibility(View.GONE);
+            tutorialview.setVisibility(View.VISIBLE);
+            viewno=2;
+        }
+        //return viewNo; done
+    }
+
+    public void backwordForwardController(int no){
+        chaptername.setText(strings.chapter[chapterOnGoing][0]);
+        tutorial.setText(strings.chapter[chapterOnGoing][1]);
+        tutorialview.setScrollY(0);
+    }
+}
---
+++ app/src/main/java/com/example/darkshadow/learnquiz/strings.java
@@ -0,0 +1,49 @@
+package com.example.darkshadow.learnquiz;
+
+/**
+ * Created by darkshadow on 3/20/18.
+ */
+
+public class strings {
+    String[][]  chapter=new String[50][2];
+    strings(){
+        chapter[0][0]="Chapter One";
+        chapter[0][1]="\n" +
+                "What is Lorem Ipsum?\n" +
+                "\n" +
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n" +
+                "Why do we use it?\n" +
+                "\n" +
+                "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\n" +
+                "\n" +
+                "Where does it come from?\n" +
+                "\n" +
+                "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum  comes from a line in section 1.10.32.";
+        chapter[1][0]="Chapter Two";
+        chapter[1][1]="\n" +
+                "What is Lorem Ipsum?\n" +
+                "\n" +
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n" +
+                "Why do we use it?\n" +
+                "\n" +
+                "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\n" +
+                "\n" +
+                "Where does it come from?\n" +
+                "\n" +
+                "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum  comes from a line in section 1.10.32.";
+        chapter[2][0]="Chapter Three";
+        chapter[2][1]="Chapter Three";
+
+        chapter[3][0]="Chapter Four";
+        chapter[3][1]="Chapter Four";
+
+        chapter[4][0]="Chapter Five";
+        chapter[4][1]="Chapter Five";
+
+        chapter[5][0]="Chapter Six";
+        chapter[5][1]="Chapter Six";
+
+        chapter[6][0]="Chapter Seven";
+        chapter[6][1]="Chapter Seven";
+    }
+}
